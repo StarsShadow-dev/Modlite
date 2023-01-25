d6328270b48bfadb85f7d1c049724873d303a00e
StarsShadow-dev <101425256+StarsShadow-dev@users.noreply.github.com>
2023-01-25 10:40:07 -0500

    added modlite.swift (unfinished)

---
+++ virtualMachine/modlite.swift
@@ -0,0 +1,6 @@
+// should this be a struct? (I do not know much Swift)
+// also how do you make things public?
+// and how do I do the equivalent of `const binaryCodes = {...}` is JavaScript
+struct ModliteRunTime {
+
+}
