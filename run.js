ab28557fb6304304aa3db5257b820793ed580940
StarsShadow-dev <101425256+StarsShadow-dev@users.noreply.github.com>
2023-01-31 12:39:41 -0500

    experimenting with the JavaScript just in time compiler

--- run.js
+++ run.js
@@ -9,4 +9,56 @@ runTime.exposedFunctions.print = () => {
 
 const opCode = fs.readFileSync("test/program.cmodlite", "utf8")
 
+// experimenting with the JavaScript just in time compiler
+
+console.time("opCode executed in")
 runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+console.time("opCode executed in")
+runTime.run(opCode)
+console.timeEnd("opCode executed in")
+
+// basically a JavaScript equivalent of the opCode
+console.time("js executed in")
+main()
+function main() {
+	console.log("[print] in main")
+	test()
+}
+function test() {
+	console.log("[print] in test")
+}
+console.timeEnd("js executed in")
